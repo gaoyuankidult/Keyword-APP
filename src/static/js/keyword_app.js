a81f71844b59b078e7ecc22beb49c319cbe0381a
Kalle Ilves <kalle.ilves@helsinki.fi>
2014-06-27 11:29:32 +0300

    Update keyword_app.js

--- src/static/js/keyword_app.js
+++ src/static/js/keyword_app.js
@@ -179,7 +179,7 @@ KeywordApp.controller("KeywordController", ["$scope", function($scope){
 		
 		$scope.keyword_suggestions = [];
 
-		$.get("172.17.72.146:8888/keyword_suggestions", function(data){
+		$.get("172.17.72.146:8888/search", function(data){
 			console.log(data);
 
 			/*data.keywords.forEach(function(keyword){
